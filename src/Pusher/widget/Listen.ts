236644ccbd1b8cf2c3f77a7c3bf753519bf697f0
raffiar <raffi.hovhannisyan@mendix.com>
2022-07-13 11:09:33 +0200

    Revert "Merge pull request #44 from stephanbruijnis/patch-2"
    This reverts commit 514d4e7fdac5a1e52b8ddbd751f6df051b4fa878, reversing
    changes made to 06074dacb89762491d9ffa55fd994d7954d64262.

--- src/Pusher/widget/Listen.ts
+++ src/Pusher/widget/Listen.ts
@@ -42,8 +42,8 @@ class NotifyListen extends WidgetBase {
                     this.showError("Authentication key and cluster are required. Please make sure Pusher.Pusher_Key and Pusher.Pusher_Cluster constants are set.");
                     return;
                 }
-                if ([ "mt1", "us2", "us3", "eu", "ap1", "ap2", "ap3", "ap4", "sa1" ]. indexOf(keyData.cluster) === -1) {
-                    this.showError(`Authentication cluster "${keyData.cluster}" is not supported. Please make sure  Pusher.Pusher_Cluster constants are set to "mt1", "us2", "us3", "eu", "ap1", "ap2", "ap3", "ap4" or "sa1");
+                if ([ "eu", "mt1", "us2", "ap1", "ap2" ]. indexOf(keyData.cluster) === -1) {
+                    this.showError(`Authentication cluster "${keyData.cluster}" is not supported. Please make sure  Pusher.Pusher_Cluster constants are set to "eu", "mt1", "us2", "ap1" or "ap2"`);
                     return;
                 }
                 this.pusher = new Pusher(keyData.key, {
